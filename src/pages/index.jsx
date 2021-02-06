0b7e826256e85a230fd0b201f8ace0e33881c050
fletchcoding <fletch_2009@hotmail.com>
2021-02-07 08:46:30 +1100

    functional firestore read added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,12 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import View from "../components/View"
 import Status from "../components/Status"
+import firebase from "gatsby-plugin-firebase"
+import { getPlaces } from "../utils/places"
 
-const Index = () => (
+const Index = () => {
+  getPlaces(firebase);
+ return (
   <Layout>
     <View title="Firebase Authentication Starter">
       <p>
@@ -28,12 +32,12 @@ const Index = () => (
       <p>
         This is the public home, and here it is a private route to start:
         <br/>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline float-right" type="button">
+        <button type="button">
           <Link to="/app/profile">Go to your profile</Link>
         </button>
       </p>
     </View>
   </Layout>
-)
+)}
 
 export default Index
